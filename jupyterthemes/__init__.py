b4c5289ad04664c245e8dc85c1c84e7b976e2fe8
Kyle Dunovan <dunovank@gmail.com>
2016-07-07 13:39:51 -0400

    fix border-radius issues; version bump 0.4.9

--- jupyterthemes/__init__.py
+++ jupyterthemes/__init__.py
@@ -9,7 +9,7 @@ import shutil
 import argparse
 from glob import glob
 from tempfile import mkstemp
-__version__ = '0.4.8'
+__version__ = '0.4.9'
 
 jnb_config_dir = jupyter_config_dir()
 HOME = os.path.expanduser('~')
