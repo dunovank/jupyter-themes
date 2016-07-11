abc4e278addd74cf7ae40ae4bbe3e251471ccd55
dunovank <dunovank@gmail.com>
2016-07-11 17:29:15 -0400

    v0.5.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='jupyterthemes',
-    version='0.5.4',
+    version='0.5.5',
     packages=['jupyterthemes'],
     include_package_data=True,
     package_data={'jupyterthemes': ['sandbox/*.js', 'layout/*.less', 'styles/*.less']},
