460026e669409aca14b2359c1c13defc05040221
dunovank <dunovank@gmail.com>
2016-08-24 18:49:56 -0400

    fixed debug input visibility; pbars with labels on top; minor patches; updated compiled themes

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ datafiles[pkgname].extend(fontsdata)
 
 setup(
     name='jupyterthemes',
-    version='0.11.7',
+    version='0.11.8',
     packages=['jupyterthemes'],
     include_package_data=True,
     package_data = datafiles,
@@ -33,7 +33,7 @@ setup(
     long_description=longdescr,
     license='MIT',
     url='https://github.com/dunovank/jupyter-themes/',
-    download_url='https://github.com/dunovank/jupyter-themes/tarball/v0.11.7',
+    download_url='https://github.com/dunovank/jupyter-themes/tarball/v0.11.8',
     author='dunovank',
     author_email='dunovank@gmail.com',
     classifiers=[
